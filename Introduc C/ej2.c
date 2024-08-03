43a14fb08971dfc685d5c86a43ffc077224ba8c0
viczza-cas321 <victorcastro1735@gmail.com>
2024-08-02 21:30:49 -0300

    ej2.c

--- Introduc C/ej2.c
+++ Introduc C/ej2.c
@@ -1,7 +1,19 @@
-#include <stdio.h> 
+#include <stdio.h>
 int main(){
-	printf("hola mundo");
-	return 0;
+        float gradf, gradc;
+        int upper, lower, steps;
+        upper=300;
+        lower=0;
+        steps=1;
+        gradf=lower;
+        printf("Farhenheit\tCelsius\n");
+        while(gradf<=upper){
+                gradc= (5/9.0)*(gradf-32);
+                printf("%6.0f %16.2f\n", gradf, gradc);
+                gradf+=steps;
+        }
+        return 0;
 
 }
+
 
